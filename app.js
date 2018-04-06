a0892a9b33f42b0cf033c6cb67b5b65e7b32f8b2
sjffly <1982764150@qq.com>
2018-04-06 10:23:54 +0800

    init project

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const app = express()
+const mongoose = require('mongoose')
+const connect = require('./model/connect')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'static/dist/')))
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+var server = app.listen(8000, function () {
+  connect()
+  console.log('Server start at 127.0.0.1:8000')
+})
---
+++ model/connect.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+
+function connect () {
+  mongoose.connect('mongodb://localhost/birthdayCollection')
+
+  var db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', function () {
+    console.log('connection has been established')
+  })
+}
+
+module.exports = connect
