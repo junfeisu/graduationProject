c0d618d8a29169ecf150a62a64addf567bc0742a
sjffly <1982764150@qq.com>
2018-04-11 13:00:42 +0800

    fix the bug of validate

--- models/validate.js
+++ models/validate.js
@@ -1,41 +1,54 @@
-function judgeType () {
-  var paramType = Object.prototype.toString.call(param)
-  return paramType.substring(8, paramType.length - 1).toLowerCase() === type
+function judgeType (param) {
+  return function (type) {
+    var paramType = Object.prototype.toString.call(param)
+    return paramType.substring(8, paramType.length - 1).toLowerCase() === type
+  }
 }
 
-function Validator (key, value) {
+function _Validator (key, value) {
+  this.stop = false
+  this.tasks = []
   this.key = key
   this.value = value
-
-  this.stop = false
-
-  this.tasks = []
   this.result = {
     valid: true,
     message: ''
   }
+  
+  let self = this
+  setTimeout(function () {
+    self.next()
+  }, 0)
 }
 
-Validator.prototype.next = function () {
+_Validator.prototype.next = function () {
   var fn = this.tasks.shift()
-  fn()
+  if (fn) {
+    fn()  
+  } else {
+    console.log('last fn')
+    console.log(this.result)
+    return this.result
+  }
 }
 
-Validator.prototype.setResult = function (message) {
+_Validator.prototype.setResult = function (message) {
   this.result.valid = false
   this.result.message = message
   this.stop = true
 }
 
-Validator.prototype.required = function () {
+_Validator.prototype.required = function () {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.value) {
-        var message = this.key + ' is required'
-        this.setResult(message)
+      console.log('required')
+      if (!self.value) {
+        var message = self.key + ' is required'
+        self.setResult(message)
       }
 
-      this.next()
+      self.next()
     }
   }())
 
@@ -44,17 +57,18 @@ Validator.prototype.required = function () {
   return this
 }
 
-Validator.prototype.string = function () {
+_Validator.prototype.string = function () {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.stop) {
-        if (!judgeType(this.value)('string')) {
-          var message = this.key + ' should be a string'
-          this.setResult(message)
+      if (!self.stop) {
+        if (!judgeType(self.value)('string')) {
+          var message = self.key + ' should be a string'
+          self.setResult(message)
         }
       }
 
-      this.next()
+      self.next()
     }
   }())
   
@@ -62,36 +76,18 @@ Validator.prototype.string = function () {
   return this
 }
 
-Validator.prototype.bool = function () {
+_Validator.prototype.bool = function () {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.stop) {
-        if (!judgeType(this.value)('boolean')) {
-          var message = this.key + ' should be a boolean'
-          this.setResult(message)
+      if (!self.stop) {
+        if (!judgeType(self.value)('boolean')) {
+          var message = self.key + ' should be a boolean'
+          self.setResult(message)
         }
       }
 
-      this.next()
-    }
-  }())
-  
-  this.tasks.push(fn)
-
-  return this
-}
-
-Validator.prototype.number = function () {
-  var fn = (function () {
-    return function () {
-      if (!this.stop) {
-        if (!judgeType(this.value)('number')) {
-          var message = this.key + ' should be a number'
-          this.setResult(message)
-        }
-      }
-
-      this.next()
+      self.next()
     }
   }())
   
@@ -100,17 +96,18 @@ Validator.prototype.number = function () {
   return this
 }
 
-Validator.prototype.array = function () {
+_Validator.prototype.number = function () {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.stop) {
-        if (!judgeType(this.value)('array')) {
-          var message = this.key + ' should be an array'
-          this.setResult(message)
+      if (!self.stop) {
+        if (!judgeType(self.value)('number')) {
+          var message = self.key + ' should be a number'
+          self.setResult(message)
         }
       }
 
-      this.next()
+      self.next()
     }
   }())
   
@@ -119,39 +116,61 @@ Validator.prototype.array = function () {
   return this
 }
 
-Validator.prototype.integer = function () {
+_Validator.prototype.array = function () {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.stop) {
-        if (!/^\d+$/.test(this.value)) {
-          var message = this.key + ' should be an integer'
-          this.setResult(message)
+      if (!self.stop) {
+        if (!judgeType(self.value)('array')) {
+          var message = self.key + ' should be an array'
+          self.setResult(message)
+        }
+      }
+
+      self.next()
+    }
+  }())
+  
+  this.tasks.push(fn)
+
+  return this
+}
+
+_Validator.prototype.integer = function () {
+  var self = this
+  var fn = (function () {
+    return function () {
+      if (!self.stop) {
+        if (!/^\d+$/.test(self.value)) {
+          var message = self.key + ' should be an integer'
+          self.setResult(message)
         }
       }
       
-      this.next()
+      self.next()
     }
   }())
 
   return this
 }
 
-Validator.prototype.min = function (minNum) {
+_Validator.prototype.min = function (minNum) {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.stop) {
-        var valueType = judgeType(this.value)
+      if (!self.stop) {
+        var valueType = judgeType(self.value)
 
-        if (valueType('string') && !this.value.lenght < minNum) {
-          let message = this.key + ' length must be at least ' + minNum + 'characters long'
-          this.setResult(message)
-        } else if (valueType('number') && !this.value < minNum) {
-          let message = this.key + ' must be larger than or equal to ' + minNum
-          this.setResult(message)
+        if (valueType('string') && self.value.length < minNum) {
+          let message = self.key + ' length must be at least ' + minNum + 'characters long'
+          self.setResult(message)
+        } else if (valueType('number') && self.value < minNum) {
+          let message = self.key + ' must be larger than or equal to ' + minNum
+          self.setResult(message)
         }
       }
       
-      this.next()
+      self.next()
     }
   }())
 
@@ -160,22 +179,23 @@ Validator.prototype.min = function (minNum) {
   return this
 }
 
-Validator.prototype.max = function (maxNum) {
+_Validator.prototype.max = function (maxNum) {
+  var self = this
   var fn = (function () {
     return function () {
-      if (!this.stop) {
-        var valueType = judgeType(this.value)
+      if (!self.stop) {
+        var valueType = judgeType(self.value)
 
-        if (valueType('string') && !this.value.length > maxNum) {
-          let message = this.key + ' length must be at least ' + maxNum + ' characters long'
-          this.setResult(message)
-        } else if (valueType('number') && !this.value < maxNum) {
-          let message = this.key + ' must be larger than or equal to ' + maxNum
-          this.setResult(message)
+        if (valueType('string') && self.value.length > maxNum) {
+          let message = self.key + ' length must be less than or equal to ' + maxNum + ' characters long'
+          self.setResult(message)
+        } else if (valueType('number') && self.value > maxNum) {
+          let message = self.key + ' must be less than or equal to ' + maxNum
+          self.setResult(message)
         }
       }
       
-      this.next()
+      self.next()
     }
   }())
   
@@ -184,28 +204,33 @@ Validator.prototype.max = function (maxNum) {
   return this
 }
 
-Validator.prototype.regex = function (reg) {
+_Validator.prototype.regex = function (reg) {
+  var self = this
   var fn = (function () {
     return function () {
       try {
-        if (!this.stop) {
+        if (!self.stop) {
           var checkReg = new RegExp(reg)
-          if (!checkReg.test(this.value)) {
-            var message = this.key + ' with value ' + this.value + ' fails to macth the pattern ' + checkReg
-            this.setResult(message)
+          if (!checkReg.test(self.value)) {
+            var message = self.key + ' with value ' + self.value + ' fails to macth the pattern ' + checkReg
+            self.setResult(message)
           }
         }
       } catch (err) {
-        this.setResult(reg + ' is not a valid regular expression')
+        self.setResult(reg + ' is not a valid regular expression')
       }
       
-      this.next()
+      self.next()
     }
   }())
 
   this.tasks.push(fn)
 
   return this
+}
+
+function Validator (key, value) {
+  return new _Validator(key, value)
 }
 
 module.exports = Validator
